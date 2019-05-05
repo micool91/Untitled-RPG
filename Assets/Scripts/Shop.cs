8ab5006da633ba4f12f493f25da785a265a02ed1
mjakobowski <micool91@gmail.com>
2019-05-05 19:31:04 +0200

    buying and selling items in vendor mechanism

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -18,6 +18,10 @@ public class Shop : MonoBehaviour
     public ItemButton[] buyItemButtons;
     public ItemButton[] sellItemButtons;
 
+    public Item selectedItem;
+    public Text buyItemName, buyItemDescription, buyItemValue;
+    public Text sellItemName, sellItemDescritpion, sellItemValue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,6 +55,8 @@ public class Shop : MonoBehaviour
 
     public void OpenBuyMenu()
     {
+        buyItemButtons[0].Press();
+
         buyMenu.SetActive(true);
         sellMenu.SetActive(false);
 
@@ -74,9 +80,16 @@ public class Shop : MonoBehaviour
 
     public void OpenSellMenu()
     {
+        sellItemButtons[0].Press();
+
         buyMenu.SetActive(false);
         sellMenu.SetActive(true);
 
+        ShowSellItems();
+    }
+
+    private void ShowSellItems()
+    {
         GameManager.instance.SortItems();
         for (int i = 0; i < sellItemButtons.Length; i++)
         {
@@ -94,5 +107,50 @@ public class Shop : MonoBehaviour
                 sellItemButtons[i].amountText.text = "";
             }
         }
+    }
+
+    public void SelectBuyItem(Item buyItem)
+    {
+        selectedItem = buyItem;
+        buyItemName.text = selectedItem.itemName;
+        buyItemDescription.text = selectedItem.description;
+        buyItemValue.text = "Value: " + selectedItem.value.ToString() + "g";
+    }
+
+    public void SelectSellItem(Item SellItem)
+    {
+        selectedItem = SellItem;
+        sellItemName.text = selectedItem.itemName;
+        sellItemDescritpion.text = selectedItem.description;
+        sellItemValue.text = "Value: " + Mathf.FloorToInt(selectedItem.value * .5f).ToString() + "g";
+    }
+
+    public void BuyItem()
+    {
+        if (selectedItem != null)
+        {
+            if (GameManager.instance.currentGold >= selectedItem.value)
+            {
+                GameManager.instance.currentGold -= selectedItem.value;
+
+                GameManager.instance.AddItem(selectedItem.itemName);
+            }
+        }
+
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
+    }
+
+    public void SellItem()
+    {
+        if(selectedItem != null)
+        {
+            GameManager.instance.currentGold += Mathf.FloorToInt(selectedItem.value * .5f);
+
+            GameManager.instance.RemoveItem(selectedItem.itemName);
+        }
+
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
+
+        ShowSellItems();
     }
 }
